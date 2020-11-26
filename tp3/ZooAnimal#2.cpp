a5d7c632a346e550c4539a58b102b3804c570fd9
YCAyca <aktas.yagmur@gmail.com>
2020-11-26 20:19:00 +0300

    Update ZooAnimal#2.cpp

--- tp3/ZooAnimal#2.cpp
+++ tp3/ZooAnimal#2.cpp
@@ -50,6 +50,7 @@ int main()
     child_elephant.Create("joe", 1001, 123, 50);
     mother_elephant.Create("ann", 1502, 100, 180);
     child_elephant.isMotherOf(mother_elephant);
+    cout << "joe's weightDate : " << child_elephant.reptWeightDate() << endl;
     cout << "the mother of joe's name is:" << mother_elephant.reptName() << endl;
     
 }
